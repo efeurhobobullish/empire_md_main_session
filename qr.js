3ecfa1e1656ffa6e0342239a9239599f58843b03
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-25 11:35:01 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -10,7 +10,7 @@ const {
     makeCacheableSignalKeyStore,
     Browsers,
     jidNormalizedUser
-} = require("baileys-mod");
+} = require("baileys");
 const { upload } = require('./mega');
 
 let router = express.Router();
