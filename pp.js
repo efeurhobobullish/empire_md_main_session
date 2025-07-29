ac4b53e6f0434798661509f28e3b70bf3b150e59
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-07-29 01:42:36 +0100

    Update pp.js

--- pp.js
+++ pp.js
@@ -25,7 +25,7 @@ app.use(express.json());
 if (fs.existsSync('./session')) fs.emptyDirSync('./session');
 
 // Upload profile picture endpoint
-app.post('/upload', upload.single('profile'), (req, res) => {
+app.post('/upload-pp', upload.single('profile'), (req, res) => {
     const file = req.file;
     if (!file) return res.status(400).send({ error: 'No file uploaded' });
 
